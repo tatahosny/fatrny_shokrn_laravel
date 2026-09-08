d48f94dba651b8cac48cf6a9643d67a451f4f51e
tatahosny <tatahosny208@gmail.com>
2026-09-08 18:22:53 +0300

    Fix blank screen: add @viteReactRefresh directive in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600;700;800;900&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 
+    @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.tsx'])
     @inertiaHead
 </head>
